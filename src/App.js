715f0a99f345c2260538a34c2fa2d00279e454bd
David M <mullaned@gmail.com>
2019-05-22 17:01:36 +0100

    using radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
+import Radium from 'radium';
 
 class App extends Component {
   state= {
@@ -45,7 +46,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+      }
     }
 
     let persons = null;
@@ -66,7 +71,11 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red'
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = ['red', 'bold'].join(' ');
@@ -95,4 +104,4 @@ class App extends Component {
   } 
 }
 
-export default App;
+export default Radium(App);

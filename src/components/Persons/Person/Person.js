ba8e976de4cbc326c80322dd3dd109024777d54b
David M <mullaned@gmail.com>
2019-05-23 16:38:14 +0100

    update lifecycle hooks for props changes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import classes from './Person.css';
 
-const person = (props) => {
-  console.log('[Person.js] rendering...');
-  return (
-    <div className={classes.Person} >
-      <p onClick={props.click}>I'm a person named {props.name}!! Who is {props.age}</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component{
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      <div className={classes.Person} >
+        <p onClick={this.props.click}>I'm a person named {this.props.name}!! Who is {this.props.age}</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
+} 
 
-export default person;
+export default Person;
